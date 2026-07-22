c000d43a7be91c2d9838ea24b19be1cf9938a64a
Jonathan20061613 <andressguilera2006@gmail.com>
2026-07-22 10:47:51 -0500

    Ejercicio 3

--- ejercicio_3/App.js
+++ ejercicio_3/App.js
@@ -1,10 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
+  const [contador, setContador] = useState(0);
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.titulo}>Contador Interactivo</Text>
+      <Text style={styles.numero}>{contador}</Text>
+      
+      <View style={styles.botonera}>
+        <TouchableOpacity 
+          style={[styles.boton, { backgroundColor: '#10B981' }]} 
+          onPress={() => setContador(contador + 1)}
+        >
+          <Text style={styles.textoBoton}>Incrementar</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity 
+          style={[styles.boton, { backgroundColor: '#0065FF' }]} 
+          onPress={() => setContador(0)}
+        >
+          <Text style={styles.textoBoton}>Reiniciar</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity 
+          style={[styles.boton, { backgroundColor: '#EF4444' }]} 
+          onPress={() => setContador(contador - 1)}
+        >
+          <Text style={styles.textoBoton}>Decrementar</Text>
+        </TouchableOpacity>
+      </View>
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +45,38 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  titulo: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    color: '#374751',
+    marginBottom: 20
+  },
+  numero: {
+    fontSize: 64,
+    fontWeight: 'bold',
+    color: '#1F2937',
+    marginBottom: 30
+  },
+  botonera: {
+    gap: 15,
+    width: '60%',
+  },
+  boton: {
+    paddingVertical: 12,
+    paddingHorizontal: 20,
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    elevation: 3,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+  },
+  textoBoton: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: '600',
+  }
 });
+  
